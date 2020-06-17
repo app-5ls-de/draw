a169946dac8bb13b04582f76836c3d63de5db30c
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-06-17 12:23:17 +0200

    auto linter (no functional changes)

--- main.js
+++ main.js
@@ -1,68 +1,68 @@
 var canvas = document.createElement("canvas")
-        var context = canvas.getContext("2d")
-        document.getElementsByClassName("canvas")[0].appendChild(canvas)
+var context = canvas.getContext("2d")
+document.getElementsByClassName("canvas")[0].appendChild(canvas)
 
-        var dpr = window.devicePixelRatio || 1 // falling back to 1
-        canvas.style.width = window.innerWidth + "px"
-        canvas.style.height = window.innerHeight + "px"
-        canvas.setAttribute("width", Math.floor(window.innerWidth * dpr))
-        canvas.setAttribute("height", Math.floor(window.innerHeight * dpr))
+var dpr = window.devicePixelRatio || 1 // falling back to 1
+canvas.style.width = window.innerWidth + "px"
+canvas.style.height = window.innerHeight + "px"
+canvas.setAttribute("width", Math.floor(window.innerWidth * dpr))
+canvas.setAttribute("height", Math.floor(window.innerHeight * dpr))
 
-        context.scale(dpr, dpr)
-        context.lineWidth = 1
-        context.lineJoin = context.lineCap = "round"
-
-
-        var up = 0
-        var down = 1
-
-        var pen = { x: -1, y: -1, state: up }
+context.scale(dpr, dpr)
+context.lineWidth = 1
+context.lineJoin = context.lineCap = "round"
 
 
-        function start(event) {
-            pen.state = down
+var up = 0
+var down = 1
+
+var pen = { x: -1, y: -1, state: up }
+
+
+function start(event) {
+    pen.state = down
+    pen.x = event.x
+    pen.y = event.y
+}
+
+function move(event) {
+    if (pen.state == down) {
+        if (Math.abs(pen.x - event.x) > 0 || Math.abs(pen.y - event.y) > 0) {
+            context.beginPath();
+            context.moveTo(pen.x, pen.y);
+            context.lineTo(event.x, event.y);
+            context.closePath();
+            context.stroke();
+
             pen.x = event.x
             pen.y = event.y
         }
+    }
+}
 
-        function move(event) {
-            if (pen.state == down) {
-                if (Math.abs(pen.x - event.x) > 0 || Math.abs(pen.y - event.y) > 0) {
-                    context.beginPath();
-                    context.moveTo(pen.x, pen.y);
-                    context.lineTo(event.x, event.y);
-                    context.closePath();
-                    context.stroke();
+function end(event) {
+    if (pen.state == down) {
+        if (event.x != 0 || event.y != 0) {
+            context.beginPath();
+            context.moveTo(pen.x, pen.y);
+            context.lineTo(event.x, event.y);
+            context.closePath();
+            context.stroke();
 
-                    pen.x = event.x
-                    pen.y = event.y
-                }
-            }
+            pen.x = event.x
+            pen.y = event.y
         }
+        context.stroke()
+        pen.state = up
+    }
+}
 
-        function end(event) {
-            if (pen.state == down) {
-                if (event.x != 0 || event.y != 0) {
-                    context.beginPath();
-                    context.moveTo(pen.x, pen.y);
-                    context.lineTo(event.x, event.y);
-                    context.closePath();
-                    context.stroke();
+canvas.addEventListener("pointerdown", start)
 
-                    pen.x = event.x
-                    pen.y = event.y
-                }
-                context.stroke()
-                pen.state = up
-            }
-        }
+canvas.addEventListener("pointermove", move)
 
-        canvas.addEventListener("pointerdown", start)
-
-        canvas.addEventListener("pointermove", move)
-
-        canvas.addEventListener("pointerup", end)
-        canvas.addEventListener("pointerenter", end)
-        canvas.addEventListener("pointercancel", end)
-        canvas.addEventListener("pointerout", end)
-        canvas.addEventListener("pointerleave", end)
+canvas.addEventListener("pointerup", end)
+canvas.addEventListener("pointerenter", end)
+canvas.addEventListener("pointercancel", end)
+canvas.addEventListener("pointerout", end)
+canvas.addEventListener("pointerleave", end)
